a16a2ddd93af684edf98f9c75ffe1863514579a1
abiramylingeswaran <abiramylingeswaran23@gmail.com>
2026-09-13 10:57:59 +0530

    folder sturcture

---
+++ src/main/java/com/example/techloom/TechloomApplication.java
@@ -0,0 +1,13 @@
+package com.example.techloom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechloomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechloomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/techloom/TechloomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techloom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechloomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
